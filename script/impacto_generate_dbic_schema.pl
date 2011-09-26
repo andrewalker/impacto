1f7a44bf6acbbee3f587b907c824586094b0b3f3
André Walker <andre@andrewalker.net>
2011-09-26 20:07:04 -0300

    script to generate DBIx::Class schema from PostgreSQL database

---
+++ script/impacto_generate_dbic_schema.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+use FindBin '$Bin';
+
+my @schemas = qw/people product finance user_account/;
+my $schema  = shift @ARGV;
+my $created = 0;
+
+map { create($_, @ARGV) } @schemas if $schema eq 'all';
+create($schema, @ARGV) if grep { $_ eq $schema } @schemas;
+
+die "Invalid schema\n" unless $created;
+
+sub create {
+    my ($schema, $db, $user, $password) = @_;
+    my $uc_schema = join( '', map { ucfirst($_) } split('_', $schema) );
+
+    $db       ||= q{''};
+    $user     ||= q{''};
+    $password ||= q{''};
+
+    system $Bin . "/impacto_create.pl model " .
+        "DB::${uc_schema} DBIC::Schema Impacto::Schema::${uc_schema} " .
+        "create=static dbi:Pg:dbname=$db $user $password db_schema=$schema";
+
+    $created++;
+}

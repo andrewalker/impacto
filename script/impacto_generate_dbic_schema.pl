f0e6a5068b7950d5d57d17d4db334d5f8c26b9a3
André Walker <andre@andrewalker.net>
2011-09-29 11:31:45 -0300

    actually, better without 'all'

--- script/impacto_generate_dbic_schema.pl
+++ script/impacto_generate_dbic_schema.pl
@@ -4,10 +4,10 @@ use strict;
 use FindBin '$Bin';
 
 my @schemas = qw/people product finance user_account/;
-my $schema  = pop @ARGV;
+my $schema  = scalar @ARGV > 3 ? pop @ARGV : undef;
 my $created = 0;
 
-map { create($_, @ARGV) } @schemas if $schema eq 'all';
+map { create($_, @ARGV) } @schemas unless defined $schema;
 create($schema, @ARGV) if grep { $_ eq $schema } @schemas;
 
 die "Invalid schema\n" unless $created;

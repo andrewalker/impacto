3406928da6e5de1e395d70304ba79f5df236ea57
André Walker <andre@andrewalker.net>
2011-11-03 19:24:41 -0200

    edit and create methods almost working

--- lib/Impacto/ControllerBase/CRUD.pm
+++ lib/Impacto/ControllerBase/CRUD.pm
@@ -32,21 +32,45 @@ sub crud_base : Chained('global_base') PathPrefix CaptureArgs(0) {
      || $self->crud_model_instance(
             $c->model( $self->crud_model_name )
         );
+    $c->stash(resultset => $rs);
+}
+
+sub crud_base_with_id : Chained('crud_base') PathPart('') CaptureArgs(1) {
+    my ( $self, $c, $id ) = @_;
+
+    $c->stash(
+        id  => $id,
+        row => $c->stash->{resultset}->find($id),
+    );
+}
+
+sub prepare_form {
+    my ( $self, $c ) = @_;
+
+    my $source = $c->stash->{resultset}->result_source;
 
     my $form_options = {
-        form => { name => $rs->result_source->from },
+        form => { name => $source->from },
         with_trigger => 1,
     };
-    $form_options->{fieldname_filter} = sub { $self->fieldname_filter(@_) }
-        if $self->can('fieldname_filter');
+    $form_options->{fieldname_filter} = sub {
+        $source->fs_field_order(@_)
+    } if $source->can('fs_field_order');
 
     my $reflector = Form::Sensible::Reflector::DBIC->new();
     $reflector->field_type_map->{text}->{defaults}->{field_class} = 'Text';
+    $reflector->field_type_map->{boolean} = {
+        defaults => { field_class => 'Toggle' },
+    };
 
-    my $form = $reflector->reflect_from($rs, $form_options);
+    my $form = $reflector->reflect_from($c->stash->{resultset}, $form_options);
+    $form->set_values({ $c->stash->{row}->get_columns() })
+        if exists $c->stash->{row};
 
     my $fs_renderer = Form::Sensible::Renderer::HTML->new({
-        additional_include_paths => [ $c->path_to(qw/root templates forms/)->stringify ],
+        additional_include_paths => [
+            $c->path_to(qw/root templates forms/)->stringify
+        ],
     });
     my $rendered_form = $fs_renderer->render($form);
     $rendered_form->display_name_delegate( FSConnector( sub { _translate_display_name($c, @_) } ) );
@@ -57,18 +81,49 @@ sub crud_base : Chained('global_base') PathPrefix CaptureArgs(0) {
     );
 }
 
-sub crud_base_with_id : Chained('crud_base') PathPart('') CaptureArgs(1) {
-    my ( $self, $c, $id ) = @_;
-
-    $c->stash(id => $id);
-}
-
 sub list      : Chained('crud_base') PathPart Args(0) {}
 sub list_json : Chained('crud_base') PathPart Args(0) {}
 sub delete    : Chained('crud_base') PathPart Args(0) {}
-sub create    : Chained('crud_base') PathPart Args(0) {}
 
-sub edit      : Chained('crud_base_with_id') PathPart Args(0) {}
+sub create : Chained('crud_base') PathPart Args(0) {
+    my ($self, $c) = @_;
+
+    $self->prepare_form($c);
+
+    if ($c->req->method eq 'POST') {
+        my $form = $c->stash->{form};
+        $form->set_values($c->req->body_params);
+
+        my $result = $form->validate();
+        if ($result->is_valid) {
+            my $values = $form->get_all_values();
+            delete $values->{submit};
+            $c->stash->{resultset}->create( $values );
+        }
+    }
+
+    $c->stash(template => 'create.tt2');
+}
+
+sub edit : Chained('crud_base_with_id') PathPart Args(0) {
+    my ($self, $c) = @_;
+    $self->prepare_form($c);
+
+    if ($c->req->method eq 'POST') {
+        my $form = $c->stash->{form};
+        $form->set_values($c->req->body_params);
+
+        my $result = $form->validate();
+        if ($result->is_valid) {
+            my $values = $form->get_all_values();
+            delete $values->{submit};
+            $c->stash->{row}->update( $values );
+        }
+    }
+
+    $c->stash(template => 'edit.tt2');
+}
+
 sub view      : Chained('crud_base_with_id') PathPart Args(0) {}
 
 sub _translate_display_name {

08b962f1e128da1434d2c980ed7609351fe44eb4
André Walker <andre@andrewalker.net>
2011-11-04 11:19:14 -0200

    fixing Parse::Dia::SQL bug when generating table

---
+++ fix_product_category.sql
@@ -0,0 +1,7 @@
+CREATE TABLE product.product_category (
+    product INTEGER NOT NULL,
+    category TEXT NOT NULL,
+    PRIMARY KEY(product, category),
+    FOREIGN KEY (product) REFERENCES product.product(id),
+    FOREIGN KEY (category) REFERENCES product.category(slug)
+);

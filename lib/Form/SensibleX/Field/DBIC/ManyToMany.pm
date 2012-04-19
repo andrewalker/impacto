172ce8404cc6bfcdabfd4d1efea4bbd1ed5d0d65
André Walker <andre@andrewalker.net>
2012-04-19 09:23:02 -0300

    added many to many

---
+++ lib/Form/SensibleX/Field/DBIC/ManyToMany.pm
@@ -0,0 +1,93 @@
+package Form::SensibleX::Field::DBIC::ManyToMany;
+
+use Moose;
+use namespace::autoclean;
+use Form::Sensible::DelegateConnection;
+use List::Util qw(first);
+
+extends 'Form::SensibleX::Field::Base::DBICSelect';
+
+has '+options_delegate' => (
+    default => sub { FSConnector( \&options_delegate_get_from_db ) },
+);
+
+has '+accepts_multiple' => (
+    default => 1
+);
+
+sub BUILD {
+    my $self = shift;
+    $self->render_hints->{render_as} = 'checkboxes';
+}
+
+sub get_rs {
+    my ( $self, $row ) = @_;
+
+    my $name     = $self->name;
+    my $accessor = "${name}_rs";
+
+    if (!$row) {
+        $row = $self->resultset->new_result({});
+        return $row->$accessor->result_source->resultset;
+    }
+
+    return $row->$accessor;
+}
+
+sub execute_search_db {
+    my ( $self, $row ) = @_;
+
+    my $rs = $self->get_rs($row);
+
+    return [
+        $rs->search(
+            $self->option_filter,
+            $self->get_options_to_search
+        )->all
+    ];
+}
+
+sub options_delegate_get_from_db {
+    my $self = shift;
+
+    return $self->get_option_hash_from_records(
+        $self->execute_search_db
+    );
+}
+
+sub get_option_hash_from_records {
+    my ($self, $records) = @_;
+
+    my $sep     = $self->option_label_separator;
+    my $name    = $self->option_label_as;
+    my $value   = $self->option_value;
+
+    return [
+        map {
+            {
+                name  => $_->concat_columns( $name,  $sep ),
+                value => _encode_value( $_, $value ),
+            }
+        } @$records
+    ];
+}
+
+sub get_values_from_row {
+    my ( $self, $row ) = @_;
+
+    my $value   = $self->option_value;
+    my $records = $self->execute_search_db;
+
+    return [ map { _encode_value( $_, $value ) } @$records ];
+}
+
+sub _encode_value {
+    my ($row, $value) = @_;
+
+    return $row->get_column( $value->[0] )
+        if scalar @$value == 1;
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
--- lib/Form/SensibleX/FieldFactory/DBIC/ManyToMany.pm
+++ lib/Form/SensibleX/FieldFactory/DBIC/ManyToMany.pm
@@ -3,13 +3,66 @@ package Form::SensibleX::FieldFactory::DBIC::ManyToMany;
 use Moose;
 use namespace::autoclean;
 use Form::Sensible::DelegateConnection;
+use Form::SensibleX::Field::DBIC::ManyToMany;
 
-extends 'Form::Sensible::Field::Select';
+has field => ( is => 'ro', isa => 'Form::SensibleX::Field::DBIC::ManyToMany' );
 
-# maybe a request and model class too?
+around BUILDARGS => sub {
+    my $orig  = shift;
+    my $class = shift;
+
+    my %field_args = ref $_[0] && ref $_[0] eq 'HASH' ? %{$_[0]} : @_;
+    my %args;
+
+    $field_args{resultset} = $field_args{model}->resultset if $field_args{model};
+    delete $field_args{model};
+    delete $field_args{request};
+
+    $args{field} = Form::SensibleX::Field::DBIC::ManyToMany->new(%field_args);
+    $args{field}{from_factory} = __PACKAGE__;
+
+    return $class->$orig(%args);
+};
 
 # form extra params:
-# categories => { many_to_many => 1, option_label => 'name', option_value => 'id' }
+# categories => { x_field_factory => 'DBIC::ManyToMany', option_label => 'name', option_value => 'id' }
+
+sub execute {
+    my ( $self, $row, $fields ) = @_;
+
+    die "multiple fields are not supported in this factory"
+        if keys %$fields > 1;
+
+    my $keys    = (values %$fields)[0];
+
+    my $name    = $self->field->name;
+    my $setter  = "set_${name}";
+    my $rs      = $self->field->get_rs;
+
+    my @records = map { $rs->find($_) } @$keys;
+
+    $row->$setter(\@records);
+
+    return 1;
+}
+
+sub get_values_from_row {
+    my ( $self, $row, $fields ) = @_;
+
+    die "multiple fields are not supported in this factory"
+        if scalar @$fields > 1;
+
+    my $field    = shift @$fields;
+    return { $field => $self->field->get_values_from_row($row) };
+}
+
+sub build_fields {
+    my $self = shift;
+
+    return (
+        [ $self->field, $self->field->name ]
+    );
+}
 
 __PACKAGE__->meta->make_immutable;
 

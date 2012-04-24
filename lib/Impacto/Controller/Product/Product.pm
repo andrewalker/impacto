39428fcb0c3990aaea8333720474939169fbae39
André Walker <andre@andrewalker.net>
2012-04-24 14:55:38 -0300

    adding product_metas

--- lib/Impacto/Controller/Product/Product.pm
+++ lib/Impacto/Controller/Product/Product.pm
@@ -6,7 +6,7 @@ BEGIN { extends 'Impacto::ControllerBase::CRUD' }
 
 sub form_columns {
     [qw/
-         name supplier cost minimum_price price weight image categories
+         name supplier cost minimum_price price weight image categories product_metas
     /]
 }
 sub form_columns_extra_params {
@@ -14,6 +14,7 @@ sub form_columns_extra_params {
         supplier => { x_field_factory => "DBIC::BelongsTo", option_label => 'person', option_value => 'person' },
         image    => { x_field_class => 'FileSelector::CatalystByteA' },
         categories => { x_field_factory => 'DBIC::ManyToMany', option_label => 'name', option_value => 'slug' },
+        product_metas => { x_field_factory => 'DBIC::RecordMeta', custom_field_name => 'name', custom_field_value => 'value' },
     }
 }
 

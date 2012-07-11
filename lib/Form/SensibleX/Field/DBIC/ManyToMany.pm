855a85bbc5afe5b28decb26b28930308f64c2cfe
André Walker <andre@andrewalker.net>
2012-07-11 20:19:50 -0300

    pod to ::ManyToMany

--- lib/Form/SensibleX/Field/DBIC/ManyToMany.pm
+++ lib/Form/SensibleX/Field/DBIC/ManyToMany.pm
@@ -34,7 +34,7 @@ sub get_rs {
     return $row->$accessor;
 }
 
-sub execute_search_db {
+sub get_records_from_db {
     my ( $self, $row ) = @_;
 
     my $rs = $self->get_rs($row);
@@ -51,7 +51,7 @@ sub options_delegate_get_from_db {
     my $self = shift;
 
     return $self->get_option_hash_from_records(
-        $self->execute_search_db
+        $self->get_records_from_db
     );
 }
 
@@ -76,7 +76,7 @@ sub get_values_from_row {
     my ( $self, $row ) = @_;
 
     my $value   = $self->option_value;
-    my $records = $self->execute_search_db($row);
+    my $records = $self->get_records_from_db($row);
 
     return [ map { _encode_value( $_, $value ) } @$records ];
 }
@@ -91,3 +91,55 @@ sub _encode_value {
 __PACKAGE__->meta->make_immutable;
 
 1;
+
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Form::SensibleX::Field::DBIC::BelongsTo - Foreign keys in Form::Sensible
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=head2 get_rs
+
+Returns the resultset to the many_to_many accessor that DBIC creates.
+
+=head2 get_option_hash_from_records
+
+Given a list of records, it returns them in an arrayref in which all the
+elements are in the form:
+
+    {
+        name  => $label,
+        value => $value,
+    }
+
+=head2 get_records_from_db
+
+Searches the database and returns the records.
+
+=head2 options_delegate_get_from_db
+
+Sub-routine handed to Form::Sensible to get the options for the select.
+
+=head2 get_values_from_row
+
+Returns the value for a given row. Useful when there are many columns in the
+value.
+
+=head2 _encode_value
+
+Encodes multiple columns for a value.
+
+=head1 AUTHOR
+
+André Walker <andre@andrewalker.net>
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify it under
+the same terms as Perl itself.

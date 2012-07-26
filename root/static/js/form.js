928fd772924002145627597f90b6017c0bdc5f34
André Walker <andre@andrewalker.net>
2012-07-26 02:31:33 -0300

    changing form's html to use dijit's widgets

--- root/static/js/form.js
+++ root/static/js/form.js
@@ -1,2 +1,8 @@
+dojo.require("dijit.form.CheckBox");
+dojo.require("dijit.form.CurrencyTextBox");
+dojo.require("dijit.form.NumberTextBox");
 dojo.require("dijit.form.TextBox");
+dojo.require("dijit.form.Textarea");
+dojo.require("dijit.form.DateTextBox");
+dojo.require("dijit.form.FilteringSelect");
 dojo.require("dijit.form.Button");

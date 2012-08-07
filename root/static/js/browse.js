c7ae59313d37ed1a56a971f12a62efde7fe1e648
André Walker <andre@andrewalker.net>
2012-08-07 19:05:30 -0300

    some love for the datagrid as well

--- root/static/js/browse.js
+++ root/static/js/browse.js
@@ -1,72 +1,50 @@
-require([
-    "dijit/form/TextBox",
-    "dijit/form/Button"
-]);
-
 var datagrid_layout;
-var datagrid_table;
-var timeout = 0;
-var last_term = '';
-var row_index;
 
 // search
-
-function search_input_keypress(e) {
-    var term = this.get('value');
-
-    if (timeout)
-        clearTimeout(timeout);
-    if (last_term == term)
-        return;
-
-    timeout = setTimeout(function () { execute_search(term) }, 700);
-}
-
-function execute_search(term) {
-    last_term = term;
-    datagrid_table.store.close();
-    datagrid_table.store.url = table_prefix_uri + '/list_json_data?q=' + encodeURI(term);
-    datagrid_table.store.fetch();
-    datagrid_table._refresh();
-    timeout = 0;
-}
-
-function delete_row(e) {
-    _delete([ datagrid_table.getItem(row_index).i._esid ]);
-}
-
-function delete_selected(e) {
-    var selected_rows = datagrid_table.selection.getSelected();
-    var ids = new Array();
-
-    for (var i = 0; i < selected_rows.length; i++)
-        ids.push(selected_rows[i].i._esid);
-
-    _delete(ids);
-}
-
-function _delete(rows) {
-    dojo.xhrPost({
-        url: table_prefix_uri + '/delete',
-        content: rows,
-        load: function(data){
-            datagrid_table.store.close();
-            datagrid_table.store.url = table_prefix_uri + '/list_json_data?q=' + encodeURI(last_term);
-            datagrid_table.store.fetch();
-            datagrid_table._refresh();
-            alert(data);
-        },
-        error: function(error){
-            alert(error);
-        }
-    });
-}
-
-// main
 require([
     "dojo",
-    "dojo/parser",
+    "dijit/registry",
+    "dojo/on",
+    "dijit/form/TextBox",
+    "dijit/form/Button",
+    "dojo/domReady!",
+],
+function (dojo, registry, on) {
+    var timeout = 0;
+    var last_term = '';
 
+    dojo.addOnLoad(function () {
+        dojo.fadeIn({
+            node: 'search'
+        }).play();
+        registry.byId('input_query').on('keyup', search_input_keypress);
+    });
+
+    function search_input_keypress(e) {
+        var term = this.get('value');
+
+        if (timeout)
+            clearTimeout(timeout);
+        if (last_term == term)
+            return;
+
+        timeout = setTimeout(function () { execute_search(term) }, 700);
+    }
+
+    function execute_search(term) {
+        last_term = term;
+        var dg_table = registry.byId('datagrid-table');
+        dg_table.store.close();
+        dg_table.store.url = table_prefix_uri + '/list_json_data?q=' + encodeURI(term);
+        dg_table.store.fetch();
+        dg_table._refresh();
+        timeout = 0;
+    }
+});
+
+// datagrid
+require([
+    "dojo",
     "dijit/registry",
 
     "dojo/dom",
@@ -80,22 +58,47 @@ require([
 
     "dojo/dom-class",
 
-    "dijit/form/TextBox",
-
     "dojox/grid/enhanced/plugins/Menu",
     "dojox/grid/enhanced/plugins/NestedSorting",
     "dojox/grid/enhanced/plugins/IndirectSelection",
     "dojox/grid/enhanced/plugins/Pagination",
     "dojo/domReady!",
 ],
-function (dojo, parser, registry, dom, on, Menu, MenuItem, Grid, Store, domclass) {
-    dojo.addOnLoad(function () {
-        dojo.fadeIn({
-            node: 'search'
-        }).play();
-    });
-
+function (dojo, registry, dom, on, Menu, MenuItem, Grid, Store, domclass) {
     // datagrid
+    var row_index;
+    var datagrid_table;
+
+    function delete_row(e) {
+        _delete([ datagrid_table.getItem(row_index).i._esid ]);
+    }
+
+    function delete_selected(e) {
+        var selected_rows = datagrid_table.selection.getSelected();
+        var ids = new Array();
+
+        for (var i = 0; i < selected_rows.length; i++)
+            ids.push(selected_rows[i].i._esid);
+
+        _delete(ids);
+    }
+
+    function _delete(rows) {
+        dojo.xhrPost({
+            url: table_prefix_uri + '/delete',
+            content: rows,
+            load: function(data){
+                datagrid_table.store.close();
+                datagrid_table.store.url = table_prefix_uri + '/list_json_data?q=' + encodeURI(last_term);
+                datagrid_table.store.fetch();
+                datagrid_table._refresh();
+                alert(data);
+            },
+            error: function(error){
+                alert(error);
+            }
+        });
+    }
 
     function datagrid_row_click_event(e) {
         if (domclass.contains(e.target, 'dojoxGridCell'))
@@ -119,6 +122,9 @@ function (dojo, parser, registry, dom, on, Menu, MenuItem, Grid, Store, domclass
 
     row_menu.startup();
     selection_menu.startup();
+
+    dojo.addClass(row_menu.domNode, 'datagridMenu');
+    dojo.addClass(selection_menu.domNode, 'datagridMenu');
 
     var datagrid_store = new Store({
         clearOnClose: true,

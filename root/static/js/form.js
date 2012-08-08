4b76f309a54b6f12bb5d6a3911ce660cd8537d7f
André Walker <andre@andrewalker.net>
2012-08-07 22:41:42 -0300

    more cleanup

--- root/static/js/form.js
+++ root/static/js/form.js
@@ -15,20 +15,9 @@ require([
     "dojo/domReady!"
 ], function (dojo, $) {
     var node = $('form.fs_form').pop();
-    var hideLoader = function(){
-        dojo.fadeOut({
-            node:"preloader",
-            duration:700,
-            onEnd: function(){
-                dojo.style("preloader", "display", "none");
-            }
-        }).play();
-    };
     dojo.addOnLoad(function () {
         dojo.fadeIn({
             node: node,
-            onEnd: function () {
-            }
         }).play();
     });
 });

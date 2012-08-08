52f8682ff10f4af0f32904b0f3d7b3e362714d24
André Walker <andre@andrewalker.net>
2012-08-08 18:29:14 -0300

    search bar

---
+++ root/static/imp/SearchBar.js
@@ -0,0 +1,35 @@
+define([
+    "dojo/_base/declare",
+    "dijit/form/TextBox",
+    "dijit/form/Button",
+    "dojo/on",
+    "dojo/domReady!",
+], function(declare, TextBox) {
+    var timeout = 0;
+    var last_term = '';
+
+    function search_input_keypress(e) {
+        var term = this.get('value');
+
+        if (timeout)
+            clearTimeout(timeout);
+        if (last_term == term)
+            return;
+
+        timeout = setTimeout(function () { execute_search(term) }, 700);
+    }
+
+    function execute_search(term) {
+        last_term = term;
+        grid.set('query', '?q=' + term);
+        timeout = 0;
+    }
+
+    return declare('imp.SearchBar', TextBox, {
+        postCreate: function () {
+            this.inherited(arguments);
+            this.on('keyup', search_input_keypress);
+        },
+    });
+
+});

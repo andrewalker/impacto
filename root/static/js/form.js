372337c5e6ffa01932874e11cc179aba446c7436
André Walker <andre@andrewalker.net>
2012-08-13 17:42:25 -0300

    no effects + replacing deprecated dojo.addOnLoad

--- root/static/js/form.js
+++ root/static/js/form.js
@@ -1,23 +1,22 @@
 require([
     "dojo",
     "dojo/query",
-    "dojo/_base/fx",
+    "dojo/ready",
     "dojo/parser",
     "dijit/form/CheckBox",
     "dijit/form/CurrencyTextBox",
     "dijit/form/NumberTextBox",
     "dijit/form/TextBox",
+    "dijit/form/ValidationTextBox",
     "dijit/form/Textarea",
     "dijit/form/DateTextBox",
     "dijit/form/FilteringSelect",
     "dijit/form/Button",
-    "dijit/Editor",
-    "dojo/domReady!"
-], function (dojo, $) {
-    var node = $('form.fs_form').pop();
-    dojo.addOnLoad(function () {
-        dojo.fadeIn({
-            node: node,
-        }).play();
+    "dijit/form/Form",
+    "dijit/Editor"
+], function (dojo, $, ready) {
+    ready(function () {
+        var node = $('form').pop();
+        dojo.style(node, 'opacity', 1);
     });
 });

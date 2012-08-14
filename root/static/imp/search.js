0524d487b98e48e144dc40fc3ac9854dce4d5183
André Walker <andre@andrewalker.net>
2012-08-13 23:14:18 -0300

    replacing dojo/ready with domReady plugin

--- root/static/imp/search.js
+++ root/static/imp/search.js
@@ -1,8 +1,7 @@
-define([ 'dojo', 'dojo/ready', 'imp/SearchBar' ],
-function (dojo, ready) {
-    ready(function () {
-        var node = dojo.byId('search');
+define([ 'dojo', 'imp/SearchBar', 'dojo/domReady!' ],
+function (dojo) {
+    var node = dojo.byId('search');
+    if (node)
         dojo.style(node, 'opacity', 1);
-    });
 });
 
